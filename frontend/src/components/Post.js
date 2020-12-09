00af3f006cada5fc42fe48364529d95eb3af5d3c
jshen1110 <jshen1110@users.noreply.github.com>
2020-12-08 22:04:30 -0800

    add grid posts part

---
+++ frontend/src/components/Post.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './Post.css';
+import PostItem from './PostItem';
+import Grid from './Grid';
+
+
+function Post() {
+  return (
+    <Grid>
+		  <PostItem
+        src= 'https://i.ibb.co/G2Fq3kh/IMG-5645.jpg'
+        title='test title'
+        price='$90'
+        path='/about'
+        content='test content'
+        staus= 'test status'
+      />
+      <PostItem
+        src= 'https://i.ibb.co/G2Fq3kh/IMG-5645.jpg'
+        title='test title'
+        price='$90'
+        path='/about'
+        content='test content'
+        staus= 'test status'
+      />
+      <PostItem
+        src= 'https://i.ibb.co/G2Fq3kh/IMG-5645.jpg'
+        title='test title'
+        price='$90'
+        path='/about'
+        content='test content'
+        staus= 'test status'
+      />
+      <PostItem
+        src= 'https://i.ibb.co/G2Fq3kh/IMG-5645.jpg'
+        title='test title'
+        price='$90'
+        path='/about'
+        content='test content'
+        staus= 'test status'
+      />
+      <PostItem
+        src= 'https://i.ibb.co/G2Fq3kh/IMG-5645.jpg'
+        title='test title'
+        price='$90'
+        path='/about'
+        content='test content'
+        staus= 'test status'
+      />
+      <PostItem
+        src= 'https://i.ibb.co/G2Fq3kh/IMG-5645.jpg'
+        title='test title'
+        price='$90'
+        path='/about'
+        content='test content'
+        staus= 'test status'
+      />
+      <PostItem
+        src= 'https://i.ibb.co/G2Fq3kh/IMG-5645.jpg'
+        title='test title'
+        price='$90'
+        path='/about'
+        content='test content'
+        staus= 'test status'
+      />
+	</Grid>
+  );
+}
+
+export default Post;
