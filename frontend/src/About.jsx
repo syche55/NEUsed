1efbecfbfe1e3bf799a9d9cd6fd31eb4fc96059f
jshen1110 <jshen1110@users.noreply.github.com>
2020-12-09 14:34:19 -0800

    add post detial modal

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Modal } from './components/Modal';
 
 export default function About() {
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+        setShowModal(prev => !prev);
+    };
     return (
-        <div style={{marginLeft: "20px"}}>
+        <div >
             <h3>NEUsed App v0.1.0</h3>
             <p>Front End: Yijing Liu, Jing Shen</p>
             <p>Back End: Yixuan Yu, Siyu Chen</p>
+            <button onClick={openModal}>test</button>
+            <Modal showModal={showModal} setShowModal={setShowModal} />
         </div>
     );
 }
