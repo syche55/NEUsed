4d86d87fc09a6b807ec19ee38896e9c2bc4f17e3
Nancy Yu <nancyyu.17@gmail.com>
2020-12-10 21:47:32 -0800

    small fix on app.js

--- backend/app.js
+++ backend/app.js
@@ -37,15 +37,15 @@ mongoose.connect("mongodb+srv://neuser:RKDo97Qk4X8lO2WH@cluster0.jeqmf.mongodb.n
   }).then(() => {
 app.listen(8000);
 
-// app.use((req, res, next) =>{
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     if(req.method === 'OPTIONS') {
-//         return res.sendStatus(200);
-//     }
-//     next();
-// });
+app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 // app.use(isAuth);
 
